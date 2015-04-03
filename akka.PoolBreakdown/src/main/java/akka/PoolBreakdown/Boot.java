542469442ef545f9148c05515902f8e63c89259e
Guilherme Renna Rodrigues <guilhermerenna@gmail.com>
2015-04-03 13:10:40 -0300

    Testing update from eclipse.

--- akka.PoolBreakdown/src/main/java/akka/PoolBreakdown/Boot.java
+++ akka.PoolBreakdown/src/main/java/akka/PoolBreakdown/Boot.java
@@ -5,7 +5,7 @@ package akka.PoolBreakdown;
 public class Boot {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world 2!");
 
 	}
 
