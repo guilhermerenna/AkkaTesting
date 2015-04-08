f90f504a20f5c979417f4cba835c6810aae7c48c
Guilherme Renna Rodrigues <guilhermerenna@gmail.com>
2015-04-08 08:39:13 -0300

    Cactus actor included.

---
+++ akka.PoolBreakdown/src/main/java/akka/PoolBreakdown/CactusActor.java
@@ -0,0 +1,23 @@
+package akka.PoolBreakdown;
+
+import Stimuli.*;
+import akka.actor.UntypedActor;
+
+public class CactusActor extends UntypedActor {
+	
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		if(arg0 instanceof LuminousStimulusMessage) {
+			System.out.println(this.getSelf().toString()+"A can see something! "+((LuminousStimulusMessage) arg0).getMessage()+"! Ref.: " + ((StimulusMessage) arg0).getSequenceNumber());
+		} else if(arg0 instanceof TouchStimulusMessage) {
+			System.out.println(this.getSelf().toString()+": "+getSender().toString()+" toched me! Spiking it back! =P Ref.: " + ((StimulusMessage) arg0).getSequenceNumber());
+			getSender().tell(new SpikeStimulusMessage("Don't touch me! I am a cactus!"),this.getSelf());
+		} else if(arg0 instanceof StimulusMessage) {
+			System.out.println("Unknown stimulus received.\n"+((StimulusMessage)arg0).getMessage()+"\nDiscarding ref. " + ((StimulusMessage) arg0).getMessage());
+		} else {
+			throw new Exception("Message type not supported.");
+		}
+		
+	}
+
+}
